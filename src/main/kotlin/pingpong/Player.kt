269dbe0ca251ba4dcbdccbbc09450a68aa66149b
yongbinkwon <yongbinkwon97@gmail.com>
2023-02-08 13:38:34 +0100

    skeleton player

---
+++ src/main/kotlin/pingpong/Player.kt
@@ -0,0 +1,13 @@
+package pingpong
+
+//skal kunne spille om ett enkelt poeng
+//har en forståelse om sitt eget ferdighetsnivå (tall fra 1-10)
+class Player {
+
+    //spille ett poeng med bortennis
+    //velg vinneren tilfeldig basert på hvor god spilleren er i forhold til motspiller
+    //returner vinneren
+    fun play(opponent: Player): Player {
+        throw NotImplementedError("haaper folk liker opplegget...")
+    }
+}
