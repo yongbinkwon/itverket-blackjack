0f22e9be0d21744de1052c33cc4928112482fb85
yongbinkwon <yongbinkwon97@gmail.com>
2023-02-08 13:44:09 +0100

    skeleton match

---
+++ src/main/kotlin/pingpong/Match.kt
@@ -0,0 +1,10 @@
+package pingpong
+
+//forstår reglene for bortennis
+class Match {
+
+    //spill en hel kamp med bordtennis trenger ikke å returnere noe.. enda
+    fun play() {
+        throw NotImplementedError("haaper folk liker maten...")
+    }
+}
