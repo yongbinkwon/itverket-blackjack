15af34c5a45cfe3582307da175f4db012b60b047
yongbinkwon <yongbinkwon97@gmail.com>
2023-02-08 17:45:34 +0100

    :)

--- src/main/kotlin/pingpong/Player.kt
+++ src/main/kotlin/pingpong/Player.kt
@@ -1,13 +1,20 @@
 package pingpong
 
+import kotlin.random.Random
+
 //skal kunne spille om ett enkelt poeng
 //har en forståelse om sitt eget ferdighetsnivå (tall fra 1-10)
-class Player {
+class Player(
+    private val skill: Int
+) {
 
     //spille ett poeng med bortennis
     //velg vinneren tilfeldig basert på hvor god spilleren er i forhold til motspiller
     //returner vinneren
     fun play(opponent: Player): Player {
-        throw NotImplementedError("haaper folk liker opplegget...")
+        return if (won(opponent)) this else opponent
     }
+
+    private fun won(opponent: Player) = Random.nextInt(skill+opponent.skill) < skill
+
 }
